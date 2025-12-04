b56fa58e452e58403f441acc8780e8d3214cff75
akfoz45 <akf.zdemir@gmail.com>
2025-12-04 22:23:25 +0300

    App update 5

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,19 +3,18 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100, verbose_name="Proje Başlığı")
     description = models.TextField(verbose_name="Açıklama")
-    image = models.ImageField(upload_to='portfolio/images/', verbose_name="Proje Görseli")
+    image = models.ImageField(upload_to='portfolio/images/', verbose_name="Proje Görseli", blank=True)
     url = models.URLField(blank=True, verbose_name="Proje Linki")
 
     def __str__(self):
         return self.title
-
 
 class Profile(models.Model):
     full_name = models.CharField(max_length=100, default="", verbose_name="Ad Soyad")
     subtitle = models.CharField(max_length=200, default="", verbose_name="Alt Başlık (Ünvan)")
     avatar = models.ImageField(upload_to='portfolio/avatar/', blank=True, null=True, verbose_name="Profil Fotoğrafı")
     about_title = models.CharField(max_length=100, default="", verbose_name="Hakkımda Başlığı")
-    about_text = models.TextField(default="", verbose_name="Hakkımda Yazısı" )
+    about_text = models.TextField(default="", verbose_name="Hakkımda Yazısı")
 
     def __str__(self):
         return "Kişisel Bilgiler (Profile)"
